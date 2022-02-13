9d0fe25530487208ba371d4906303de727c897c3
Anton Bortnikov <toha1100@gmail.com>
2022-02-13 20:29:28 +0500

    fix: 404 page for mobiles

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,10 +1,18 @@
+import Image from "next/image";
 import React from "react";
 import styles from "./styles.module.css";
+import Cat from "../../../public/cat.webp";
 
 export default function Error404() {
   return (
     <div className={styles.container}>
-      <img className={styles.img} />
+      <Image
+        src={Cat}
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+        placeholder="blur"
+      />
       <div className={styles.banner}>
         <h1 className={styles.header}>404</h1>
         <p className={styles.text}>This is not the page you are looking for</p>
